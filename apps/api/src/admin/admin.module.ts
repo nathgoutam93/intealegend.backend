0152abf1c3122035cc4f37a8efe05f0447960036
nathgoutam93 <nathgoutam93@gmail.com>
2025-03-26 18:28:17 +0530

    hotfix: mail

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService],
-  exports: [AdminService],
+  providers: [AdminService, MailService],
 })
 export class AdminModule {}
